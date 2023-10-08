843c1c1f442f4dc1341ea2d060a366206a6b4103
abmas <ashok.ramakrishnan@gmail.com>
2023-10-08 09:36:00 -0400

    Update runA.py

--- runA.py
+++ runA.py
@@ -1,25 +1,32 @@
-from hub import motor_pair, color_sensor, motor, runloop, orientation, force_sensor, distance_sensor, device, color
+from hub import port
+import runloop
+import motor_pair
+from hub import motion_sensor
 
-def gyro_follow_angle_for_distance(angle, distance):
+'''
+arg1: angle (in degrees)
+arg2: distance_to_cover (in degrees - 360 degrees is one full rotation of robot wheel)
+async def gyro_follow_angle_for_distance(angle, distance_to_cover):
     # divide distance into 10 equal parts
-    step = distance/10
-    left_velocity = 1000
-    right_velocity = 1000
-    while (step <= distance):
-        # Move straight at default velocity for 360 degrees
-        await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, step, left_velocity, right_velocity)
-        # Read the sensor values and adjust angles
-        current_sensor_values = motion_sensor.tilt_angles()
-        # Say yaw is +10, this means the robot has deviated 10 degrees. To get it back on straight line,
-        # we will have to double the skew
-        adjustment = motion_sensor.tilt_angles()[0]
-        left_velocity+=adjustment
-        right_velocity-=adjustment
-    pass
+    motion_sensor.reset_yaw(0)
+    step = int(distance_to_cover/10)
+    distance_covered = int(0)
 
-aync def main():
-    motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
-    await gyro_follow_angle_for_distance(0,300)
+    left_velocity = 300
+    right_velocity = 300
+    steering_angle:int = angle
+    while (distance_covered <= distance_to_cover):
+        # Move straight at default velocity for 360 degrees
+        motor_pair.move_for_degrees(motor_pair.PAIR_1, step, steering_angle)
+        # Read the sensor values and adjust angles
+        steering_angle = (motion_sensor.tilt_angles()[0] * -1) + angle
+        distance_covered += step
+        print("steering_angle=", steering_angle)
+    print("DONE")
+
+async def main():
+    motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)    
+    await gyro_follow_angle_for_distance(0, 360)
+
 
 runloop.run(main())
-
