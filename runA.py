e6a207d0778d18388192b360086fc05f0ac3690c
oveep <oveepimpalwadkar@gmail.com>
2023-10-09 08:44:34 -0400

    Working version of runA.py on 10/08/2023

--- runA.py
+++ runA.py
@@ -3,30 +3,33 @@ import runloop
 import motor_pair
 from hub import motion_sensor
 
-'''
-arg1: angle (in degrees)
-arg2: distance_to_cover (in degrees - 360 degrees is one full rotation of robot wheel)
+
+# arg1: angle (in degrees)
+# arg2: distance_to_cover (in degrees - 360 degrees is one full rotation of robot wheel)
 async def gyro_follow_angle_for_distance(angle, distance_to_cover):
     # divide distance into 10 equal parts
     motion_sensor.reset_yaw(0)
     step = int(distance_to_cover/10)
     distance_covered = int(0)
 
-    left_velocity = 300
-    right_velocity = 300
+    
     steering_angle:int = angle
     while (distance_covered < distance_to_cover):
         # Move straight at default velocity for 360 degrees
+        print("BEFORE motion_sensor.tilt_angles=", motion_sensor.tilt_angles()[0])
         await motor_pair.move_for_degrees(motor_pair.PAIR_1, step, steering_angle)
         # Read the sensor values and adjust angles
         steering_angle = (motion_sensor.tilt_angles()[0] * -1) + angle
+        motion_sensor.reset_yaw(0)
+        print("motion_sensor.tilt_angles=", motion_sensor.tilt_angles()[0])
         distance_covered += step
+        print("distance_covered=", distance_covered)
         print("steering_angle=", steering_angle)
     print("DONE")
 
 async def main():
-    motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)    
-    await gyro_follow_angle_for_distance(0, 360)
+    motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
+    await gyro_follow_angle_for_distance(0, 1000)
 
 
 runloop.run(main())
