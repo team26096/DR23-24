66e7b6e3384b9f52cec0b8d70426a0daeb0e2d8c
abmas <ashok.ramakrishnan@gmail.com>
2023-10-15 16:24:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import runloop
 import motor_pair
 from hub import motion_sensor
 
+STEERING_CORRECTION=1
+
 """
 This function turns the gyro in place to specified deci degrees (-1800 to 1800)
 This is achieved by setting a steering value of -100 or + 100 based on which
@@ -44,14 +46,12 @@ def follow_angle_for_distance(angle, distance_to_cover):
         current_yaw = motion_sensor.tilt_angles()[0]
         if current_yaw < angle:
             # steer slightly to the right
-            motor_pair.move(motor_pair.PAIR_1,steering_correction)
+            motor_pair.move(motor_pair.PAIR_1,STEERING_CORRECTION)
         elif current_yaw > angle:
-            motor_pair.move(motor_pair.PAIR_1,-steering_correction)
-        #await runloop.sleep_ms(20)
-
+            motor_pair.move(motor_pair.PAIR_1,-STEERING_CORRECTION)
     motor_pair.stop(motor_pair.PAIR_1)
     print("Total distance travelled = %d", distance_covered)
-    print("Gyro move for distance DONE")
+    print("Gyro follow angle for distance DONE")
     
 """
 The function moves the robot for distance requested, and uses the gyro sensor to keep a
@@ -62,27 +62,9 @@ Usually a low steering correction is sufficient.
     be a low value, otherwise the movement could be erratic. recommended values are 1
     to 10.
 """
-async def gyro_assisted_move_for_distance(distance_to_cover, steering_correction):
-    # get initial reading from left motor
-    initial_position = abs(motor.relative_position(port.A))
-    print(initial_position)
-    distance_covered = 0
-    motor_pair.move(motor_pair.PAIR_1,0)
-
-    while (distance_covered < distance_to_cover):
-        current_position = abs(motor.relative_position(port.A))
-        distance_covered = current_position - initial_position
-        current_yaw = motion_sensor.tilt_angles()[0]
-        if current_yaw < 0:
-            # steer slightly to the right
-            motor_pair.move(motor_pair.PAIR_1,steering_correction)
-        elif current_yaw > 0:
-            motor_pair.move(motor_pair.PAIR_1,-steering_correction)
-        #await runloop.sleep_ms(20)
-
-    motor_pair.stop(motor_pair.PAIR_1)
-    print("Total distance travelled = %d", distance_covered)
-    print("Gyro move for distance DONE")
+async def gyro_assisted_move(angle, stop_function, **kwargs):
+    stop_function(angle, **kwargs)
+     
 
 async def do_3d_cinema(angle, distance_to_cover):
     # Move straight at default velocity for 360 degrees
@@ -96,7 +78,7 @@ async def main():
     motion_sensor.reset_yaw(0)
     # Turn the robot 45 degrees (450 decidegrees) in place
     await gyro_in_place_turn_for_decidegrees(450)
-    await gyro_assisted_move_for_distance(720,1)
+    await gyro_assisted_move(0, follow_angle_for_distance, distance_to_cover=720)
     #await do_3d_cinema(0, 360)
 
 runloop.run(main())
