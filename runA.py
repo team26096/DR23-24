a8f51ffee6afb5539533a0aa6d355a33651ba5aa
abmas <ashok.ramakrishnan@gmail.com>
2023-10-15 16:10:30 -0400

    Update runA.py - temp changes

--- runA.py
+++ runA.py
@@ -10,25 +10,49 @@ This is achieved by setting a steering value of -100 or + 100 based on which
 side we want to turn.
 """
 async def gyro_in_place_turn_for_decidegrees(decidegrees):
-    # set yaw face and reset yaw
-    motion_sensor.set_yaw_face(motion_sensor.FRONT)
-    motion_sensor.reset_yaw(0)
-    initial_yaw = 0
+    initial_yaw = motion_sensor.tilt_angles()[0]
+    target_yaw = initial_yaw + decidegrees
     if decidegrees > 0:
         steering = 100
+        # since steering is 100, the yaw value will increase.
+        # so 'move' until the current value is less than target_yaw
+        while motion_sensor.tilt_angles()[0] < target_yaw:
+            motor_pair.move(motor_pair.PAIR_1,steering)
     elif decidegrees < 0:
         steering = -100
+        # since steering is -100, the yaw value will decrease.
+        # so 'move' until the current value is greater than target_yaw
+        while motion_sensor.tilt_angles()[0] > target_yaw:
+            motor_pair.move(motor_pair.PAIR_1,steering)
     else:
         # if decidegrees is 0, there is nothing to do!
         return
-    while abs(motion_sensor.tilt_angles()[0]) < abs(decidegrees):
-        motor_pair.move(motor_pair.PAIR_1,steering)
-    
     motor_pair.stop(motor_pair.PAIR_1)
-    # reset the yaw again, so where we are facing is the new 0!
-    motion_sensor.reset_yaw(0)
 
+def follow_forever():
+    return True
 
+def follow_angle_for_distance(angle, distance_to_cover):
+    # get initial reading from left motor
+    initial_position = abs(motor.relative_position(port.A))
+    # print(initial_position)
+    distance_covered = 0
+    motor_pair.move(motor_pair.PAIR_1,0)
+    while (distance_covered < distance_to_cover):
+        current_position = abs(motor.relative_position(port.A))
+        distance_covered = current_position - initial_position
+        current_yaw = motion_sensor.tilt_angles()[0]
+        if current_yaw < angle:
+            # steer slightly to the right
+            motor_pair.move(motor_pair.PAIR_1,steering_correction)
+        elif current_yaw > angle:
+            motor_pair.move(motor_pair.PAIR_1,-steering_correction)
+        #await runloop.sleep_ms(20)
+
+    motor_pair.stop(motor_pair.PAIR_1)
+    print("Total distance travelled = %d", distance_covered)
+    print("Gyro move for distance DONE")
+    
 """
 The function moves the robot for distance requested, and uses the gyro sensor to keep a
 straight line. It uses the yaw value to adjust steering (if needed) to correct direction.
@@ -39,13 +63,10 @@ Usually a low steering correction is sufficient.
     to 10.
 """
 async def gyro_assisted_move_for_distance(distance_to_cover, steering_correction):
-    # set yaw face and reset yaw
-    motion_sensor.set_yaw_face(motion_sensor.FRONT)
-    motion_sensor.reset_yaw(0)
     # get initial reading from left motor
     initial_position = abs(motor.relative_position(port.A))
     print(initial_position)
-    distance_covered = 0;
+    distance_covered = 0
     motor_pair.move(motor_pair.PAIR_1,0)
 
     while (distance_covered < distance_to_cover):
@@ -71,9 +92,16 @@ async def do_3d_cinema(angle, distance_to_cover):
 
 async def main():
     motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
+    motion_sensor.set_yaw_face(motion_sensor.FRONT)
+    motion_sensor.reset_yaw(0)
     # Turn the robot 45 degrees (450 decidegrees) in place
     await gyro_in_place_turn_for_decidegrees(450)
     await gyro_assisted_move_for_distance(720,1)
     #await do_3d_cinema(0, 360)
 
 runloop.run(main())
+
+# Need the following...
+# 1. do not reset yaw inside gyro angle follow. the angle value passed in is the reference yaw. just maintain it.
+# have a function pointer as argument, which will be the stop condition. follow_angle_forever, follow_angle_for_time, follow_angle_for_distance,
+# follow_angle_until_left_black, fllow_angle_until_left_white, follow_angle_until_right_black, follow_angle_until_right_white
