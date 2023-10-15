adde9fbf596c57f5a94043664c58d0d8e46f5606
oveep <oveepimpalwadkar@gmail.com>
2023-10-15 14:31:56 -0400

    Once a button press is detected, ignore other button press

--- run_hat_left.py
+++ run_hat_left.py
@@ -9,13 +9,13 @@ def gyro_follow_angle_for_distance(angle, distance_to_cover):
     # TODO
     pass
 
+
 def run_motor(port_name, velocity=1000):
-    # TODO: Add velocity parameter
     motor.run(port_name, velocity)
+
 
 def stop_motor(port_name):
     motor.stop(port_name)
-
 
 
 def run_motor_while_button_pressed(motor_port, velocity = 1000):
@@ -23,14 +23,20 @@ def run_motor_while_button_pressed(motor_port, velocity = 1000):
     while not button.pressed(button.LEFT) and not button.pressed(button.RIGHT):
         pass
     
-    while button.pressed(button.LEFT) or button.pressed(button.RIGHT):
+    if button.pressed(button.LEFT) or button.pressed(button.RIGHT):
         # Determine which button is pressed
-        button_name = button.LEFT if button.pressed(button.LEFT) else button.RIGHT
+        target_button = button.LEFT if button.pressed(button.LEFT) else button.RIGHT
         
-        if button_name == button.LEFT:
-            velocity = -1000
+        # Reverse velocity (to Anti-clockwise) if left button is pressed
+        if target_button == button.LEFT:
+            velocity = velocity * -1
         
         run_motor(motor_port, velocity)
+        # Run motor until same button is released
+        
+        while button.pressed(target_button):
+            pass
+        
     stop_motor(motor_port)
 
 # ----------------------------------------
