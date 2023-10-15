c59a5f3583c504ec606d1e9dea6b891256c74715
svararamakrishnan <146660089+svararamakrishnan@users.noreply.github.com>
2023-10-15 11:01:41 -0400

    Update runA.py
    we changed the steering angle to -10 so the left motor will get 90 percent power and the right motor will get 100 percent power

--- runA.py
+++ runA.py
@@ -9,18 +9,23 @@ from hub import motion_sensor
 #arg2: distance_to_cover (in degrees - 360 degrees is one full rotation of robot wheel)
 """
 async def gyro_follow_angle_for_distance(angle, distance_to_cover):
+    # set yaw face and reset yaw
+    motion_sensor.set_yaw_face(motion_sensor.FRONT)
+    motion_sensor.reset_yaw(0)
     # get initial reading from left motor
     initial_position = abs(motor.relative_position(port.A))
     print(initial_position)
     distance_covered = 0;
-    # Move motor pair at constant velocity util further command is given
-    motor_pair.move(motor_pair.PAIR_1,0)
+    # Move motor pair at constant velocity util further command is given at angle provided 
+    # angle can be -100-100. A value of -100 will give 0 power to left motor and 100 percent
+    # power to right motor.
+    motor_pair.move(motor_pair.PAIR_1,angle)
 
     while (distance_covered < distance_to_cover):
         current_position = abs(motor.relative_position(port.A))
         distance_covered = current_position - initial_position
         print(current_position)
-        await runloop.sleep_ms(20)
+        #await runloop.sleep_ms(20)
 
     motor_pair.stop(motor_pair.PAIR_1)
     print("Total distance travelled = %d", distance_covered)
@@ -34,7 +39,7 @@ async def do_3d_cinema(angle, distance_to_cover):
 
 async def main():
     motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
-    await gyro_follow_angle_for_distance(0, 720)
+    await gyro_follow_angle_for_distance(-10, 720)
     #await do_3d_cinema(0, 360)
 
 runloop.run(main())
