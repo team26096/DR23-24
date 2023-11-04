c9f7e2ff45666bca3bc8258c0ed58e8cf73a40e7
SaanviNan <114314629+SaanviNan@users.noreply.github.com>
2023-11-04 17:12:18 -0400

    Update princess.py
    RunD changes with Svara and Ovee. RunD completed dropping audience member and expert and positioned for light & sound show.

--- princess.py
+++ princess.py
@@ -17,18 +17,37 @@ def degreesForDistance(distance_cm):
     return int((distance_cm/WHEEL_CIRCUMFERENCE) * 360)
 
 yaw = 0
+cs_left = 0
+cs_right = 0
+
 def get_all_values():
     global cs_left, cs_right, yaw
     cs_left = color_sensor.reflection(port.D)
     cs_right = color_sensor.reflection(port.F)
-    yaw = motion_sensor.tilt_angles()[0] * -1
+    yaw = motion_sensor.tilt_angles()[0] * -0.1
     print("yaw={}", yaw)
+    #print("csl={}", cs_left)
+    #print("csr={}", cs_right)
+
 
 def wait_for_yaw(angle=90):
     if angle > 0:
         while yaw <= angle: get_all_values()
     elif angle < 0:
         while yaw >= angle: get_all_values()
+
+def wait_for_left_color(clr=0):
+    cs_left = color_sensor.reflection(port.D)
+    print("wflc entry csl={}", cs_left)
+    if clr == 0:
+        while cs_left > 20: 
+            cs_left = color_sensor.reflection(port.D)
+            runloop.sleep_ms(100)
+    else:
+        while cs_left < 90:
+            cs_left = color_sensor.reflection(port.D)
+            runloop.sleep_ms(100)
+    print("wflc exit csl={}", cs_left)
 
 def spin_gyro_turn(steer=100, speed=50, angle=90, stop=False):
     motion_sensor.reset_yaw(0)
@@ -42,13 +61,19 @@ def pivot_gyro_turn(left_speed=0, right_speed=50, angle=90, stop=False):
     wait_for_yaw(angle=angle)
     if stop: motor_pair.stop(motor_pair.PAIR_1)
 
-# Load program wait --------------------
-# print('waiting to start')
-# light.color(light.POWER, color.BLUE) #blue indicates program loaded and ready
-# runloop.until(button.pressed(button.LEFT)) #press left button to start program
-# runloop.sleep_ms(500)
-# light_matrix.show_image(light_matrix.IMAGE_DIAMOND) #program starting to run
-# light.color(light.POWER, color.GREEN)
+def move_until_left_black(steer=100, speed=50, angle=90, stop=False):
+    print("Inside move until black")
+    motor_pair.move(motor_pair.PAIR_1, steer, velocity=speed, acceleration=100)
+    wait_for_left_color(0)
+    if stop: motor_pair.stop(motor_pair.PAIR_1)
+    print("Done with move until black")
+
+def move_until_left_white(steer=100, speed=50, angle=90, stop=False):
+    print("Inside move until white")
+    motor_pair.move(motor_pair.PAIR_1, steer, velocity=speed, acceleration=100)
+    wait_for_left_color(1)
+    if stop: motor_pair.stop(motor_pair.PAIR_1)
+    print("Done with move until white")
 
 # Main programs --------------------
 async def runA():
@@ -58,4 +83,20 @@ async def runA():
     pivot_gyro_turn(80, 0, 900, True)
     #sys.exit("Finished")
 
-runloop.run(runA())
+async def runD():
+    print("Inside runD")
+    motor.run_for_degrees(port.C, 600, -500) # move rack left
+    motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
+    move_until_left_black(0, 200, 0, True)
+    move_until_left_white(0, 200, 0, True)
+    move_until_left_black(0, 200, 0, True)
+    move_until_left_white(0, 200, 0, True)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(12), 0)
+    move_until_left_black(0, -80, 0, True)
+    # move_until_left_white(0, -80, 0, True)
+    # move_until_left_black(0, -80, 0, True)
+    pivot_gyro_turn(0, -80, 45, True)
+    motor.run_for_degrees(port.C, 600, 500) # move rack right
+    print("Done with runD")
+
+runloop.run(runD())
